eec2246b963ab2867275d448212d59afe8a3d3e8
Adam Preston <adam.m.preston@gmail.com>
2018-11-17 20:25:40 -0700

    only deploy templates where they are needed

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-zip -r queue-jap-export.zip queue.js package.json package-lock.json node_modules/
+zip -r queue-jap-export.zip queue.js package.json package-lock.json node_modules/ 
 
 aws lambda update-function-code --function-name queue-jap-export --zip-file fileb://queue-jap-export.zip
 
-zip -r process-jap-export.zip process.js package.json package-lock.json node_modules/
+zip -r process-jap-export.zip process.js package.json package-lock.json node_modules/ templates/
 
 aws lambda update-function-code --function-name process-jap-export --zip-file fileb://process-jap-export.zip
